0a85f9f5eaff480325b4c06a4462ecc641a1cc28
lastingParadox <zanpres@gmail.com>
2021-09-06 17:04:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# firstProgram
+  Testin Github
+  ---------------------
+  Gives the accounts of "Jane Green" and "John Blue" and lets the user add balances to them.
---
+++ src/main/java/Account.java
@@ -0,0 +1,35 @@
+/*
+ * UCF COP3330 Fall 2021 firstProgram
+ * Copyright 2021 Zander Preston
+ */
+
+public class Account {
+    private String name;
+    private double balance;
+
+    public Account(String name, double balance) {
+        this.name = name;
+
+        if (balance > 0.0) {
+            this.balance = balance;
+        }
+    }
+
+    public void deposit(double depositAmount) {
+        if (depositAmount > 0.0) {
+            balance = balance + depositAmount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
